49a665d7bfc4ccb4819f4afb9fb160ff8d925f87
keithbooher <keith.booher@yahoo.com>
2020-09-27 21:41:00 -0500

    strapi promotion

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -26,10 +26,13 @@ const Home = ({ mobile }) => {
     }
   }
   return (
-    <div style={containerStyle}>
-      <h1>Hi, Im Keith!</h1>
-      <h1>I develop websites and applications</h1>
-      <h2>I value intuitiveness and creativity</h2>
+    <div>
+      <div style={containerStyle}>
+        <h1>Hi, Im Keith!</h1>
+        <h1>I develop websites and applications</h1>
+        <h2>I value intuitiveness and creativity</h2>
+      </div>
+      <h6 style={{ position: "absolute", bottom: "0px", left: "50%", transform: "translate(-50%, 0)" }}>Content is provided using the Strapi headless CMS!</h6>
     </div>
   )
 }
